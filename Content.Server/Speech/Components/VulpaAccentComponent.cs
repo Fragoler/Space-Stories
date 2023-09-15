b3caa73278f3ee78fe326f280cc1cc20886286e4
Doublechest <twin01v@gmail.com>
2023-09-15 16:11:19 +0300

    fix

--- Content.Server/Speech/Components/VulpaAccentComponent.cs
+++ Content.Server/Speech/Components/VulpaAccentComponent.cs
@@ -1,7 +1,7 @@
 namespace Content.Server.Speech.Components
 {
     [RegisterComponent]
-    public sealed class VulpaAccentComponent : Component
+    public sealed partial class VulpaAccentComponent : Component
     {
         public string Accent = default!;
     }
